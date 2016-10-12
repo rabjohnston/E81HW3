b76ec8c00cc1c7b425a27a84a4d3310f8e5802ac
Rob <rob@monach.com>
2016-10-12 16:51:44 +0800

    Add np array files for class 4

--- HW3ScratchpadSplit.py
+++ HW3ScratchpadSplit.py
@@ -42,7 +42,7 @@ def preprocessFeatures3( X ):
         X.loc[X[col].isnull(), col] = xMean
     
     # Normalise the data attributes
-    #X = (X - X.mean(axis=0)) /  X.std(axis=0)
+    X = (X - X.mean(axis=0)) /  X.std(axis=0)
     
     return (X)
     
@@ -58,67 +58,34 @@ def extractFeatures( X, nFeatures, ranking ):
     
 def featureSelection( X ):
 
-
+    clfName = 'rf'
     X_features = []
 
     class1_nFeatures = 158
-    class1_ranking = np.array(\
-    [110,   1,  24,  54,   1,   1,   1, 160,   1,  44,   1,  64, 120, 101,   1,   1, 177,   1,
-       1,  61, 127,   1, 147,   1,   1, 119,  22,   2,   1,   1,  47,   4, 145, 138,  83,   1,
-     126,  94,  35,   1, 129,   1,   1,  23,  86,   1,   1,   1,   1,   1,  66,   1, 122,   9,
-     104,  90, 134, 128,   1,   1,  27, 168,   5,  80,   1, 167,   1, 174, 117,   1,  26,   1,
-      31,  48,   1,   1, 149,   1,   1,   1,  52,  79, 112,  95,   1,  42,   1,   1,   1,   1,
-      81,   1,   1,  91,  89,  29,   1, 170,   1,   1,   1,   1,  15,  97, 140, 107,   1, 102,
-       1,  11, 146, 113, 108, 109,   1, 152,   1,  17,   1,   1,  73,   1,  40,   1,  43,   1,
-     151,   1,   1,   1, 105,  63,   1,   1,   1, 144,   1,   1,   1,   1,   8,   1,   1, 103,
-     125, 131,   1,   1,   1,  78,  51,  82, 164,   1,   1,   1,   1,   1, 165, 121,   1, 159,
-       1,   1,  37,  21,   1,   1,  93,  38,   1, 153,  14, 130,   1, 172,   1, 136,   1,   7,
-      84, 123,   1,  71,  34, 141, 135,  92,  18, 124,  74, 143,   1,   1,   1,   1,  39,   1,
-       1,   1,   1,  87,   1,   1,  19,  60, 158,   1,   1,   1,  46,   1,   1,   1, 157,   1,
-       1,  13,  69,  70,   1,   1,  62,  16, 114,  68, 111,  55,   1,   1,   1,   1,   1,   1,
-       1,   1,  76, 175,   1,  53,  10,   1,   1,   1,   1,  50,  58,   1,   1,   1,  25, 106,
-      49, 139,  65,  56,  96,   1,   1,   1,   1,   1,  41,  98,   1, 163,  72,  20,  1 , 12,
-     132, 166, 169,   1,   1,  67,  85,  36, 173,   1,   1, 116,   1,  57,  88, 162,  1 , 75,
-      30,   1,   3,   1,   1,   1,   1, 156,   1, 100,   1,   1, 171, 161,  99,   1, 155,   1,
-     142,   1,  32,   1, 150, 148,   1,  28,  77,   1, 115,   1, 137,   1,   6, 133,   1, 176,
-       1,   1, 118,  59,   1, 154,  33,   1,  45,   1])
+    class1_ranking = np.load('{}_class1_ranking.npy'.format(clfName))
 
     class2_nFeatures = 147
-    class2_ranking = np.load('rf_class2_ranking.npy')
+    class2_ranking = np.load('{}_class2_ranking.npy'.format(clfName))
     
     class3_nFeatures = 153
-    class3_ranking = np.load('rf_class3_ranking.npy')
+    class3_ranking = np.load('{}_class3_ranking.npy'.format(clfName))
     
     class4_nFeatures = 64
-    class4_ranking = np.array( \
-    [175, 73, 39, 86, 11, 1, 24, 201, 1, 3, 33, 208, 192, 166, 30, 68, 212, 1, \
-    91, 1, 119, 164, 61, 1, 1, 263, 184, 248, 83, 21, 1, 116, 182, 113, 146, 1, \
-    118, 225, 1, 261, 193, 1, 162, 48, 180, 37, 264, 36, 202, 1, 229, 70, 271, 17, \
-    85, 239, 152, 240, 5, 1, 206, 140, 51, 4, 120, 265, 69, 131, 154, 1, 1, 104, \
-    34, 49, 1, 1, 244, 57, 45, 138, 115, 233, 253, 10, 8, 260, 62, 50, 98, 29, \
-    81, 16, 189, 153, 114, 76, 1, 41, 1, 20, 179, 1, 12, 129, 128, 177, 149, 246, \
-    1, 54, 236, 1, 92, 72, 35, 64, 268, 1, 127, 1, 32, 1, 2, 22, 203, 15, \
-    42, 106, 9, 1, 6, 71, 174, 173, 79, 109, 200, 142, 169, 121, 1, 55, 144, 227, \
-    168, 1, 122, 90, 1, 1, 60, 75, 157, 96, 1, 155, 150, 7, 221, 249, 216, 159, \
-    1, 80, 250, 218, 220, 1, 102, 170, 1, 194, 195, 197, 1, 59, 181, 1, 1, 27, \
-    230, 237, 262, 1, 226, 238, 269, 231, 1, 137, 205, 255, 1, 143, 107, 44, 1, 1, \
-    130, 43, 78, 31, 1, 124, 1, 77, 171, 97, 18, 190, 1, 1, 1, 95, 223, 111, \
-    258, 163, 172, 211, 26, 185, 82, 147, 191, 84, 252, 89, 28, 13, 151, 103, 1, 145, \
-    1, 1, 110, 266, 204, 161, 67, 101, 178, 1, 188, 207, 87, 1, 58, 1, 214, 196, \
-    126, 117, 259, 257, 219, 105, 88, 108, 1, 1, 23, 198, 139, 14, 234, 245, 1, 209,
-    241, 141, 213, 47, 52, 132,  53, 167, 228,   1,   1, 254, 133, 199,  65,   1, 165, 156, \
-    1,  94, 123,  74,  40,  99, 100, 247, 222,  38, 256,   1,   1, 125, 134,  19, 210,  56, \
-    93, 160, 158, 186, 215, 176,   1, 267, 136, 112, 224,  46,  66, 187,   1, 232, 183, 217, \
-    135,  63, 243, 251, 270, 242,  25, 148, 235,   1])
+    class4_ranking = np.load('{}_class4_ranking.npy'.format(clfName))
  
     X_features.append(extractFeatures( X, class1_nFeatures, class1_ranking ))
     X_features.append(extractFeatures( X, class2_nFeatures, class2_ranking ))
     X_features.append(extractFeatures( X, class3_nFeatures, class3_ranking ))
 
     X4 = extractFeatures( X, class4_nFeatures, class4_ranking )
+    
+    print(np.any(np.isnan(X4)))
 
-    #print(X4.describe())
-    #X4 = X4.apply(np.log1p)
+
+    print(np.all(np.isfinite(X4)))
+
+    print(X4.describe())
+    X4 = X4.apply(np.log)
     X_features.append(X4)
     
     return X_features
@@ -290,8 +257,10 @@ def main():
     
     
     # Run the model
-    #Y_predict, model = runModel(X_train, X_test, Y_train, Y_test)
-    Y_predict, model = runEnsembleModel(X_train, X_test, Y_train, Y_test)
+    Y_predict, model = runModel(X_train, X_test, Y_train, Y_test)
+    #Y_predict, model = runEnsembleModel(X_train, X_test, Y_train, Y_test)
+    
+
     
     AUC = calculateROC(Y_test, Y_predict)
     
