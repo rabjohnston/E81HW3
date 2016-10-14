a3a049353976ca0077e067286df310158c3e03f4
Rob <rob@monach.com>
2016-10-14 19:28:19 +0800

    Created Submission directory and move files into it

---
+++ Submission/Ensemble.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+
+
+from sklearn.metrics import roc_curve, auc
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC # "Support Vector Classifier"
+
+
+class Ensemble:
+    
+    def __init__(self, attrs):
+        self.attrs = attrs
+        
+    def run(self, verbose):
+        if verbose > 0:
+            print("Running ensemble")
+
+        #Build Model1 - Level 0    
+        if verbose > 0:
+            print("Running Random Forest Classifier")
+            
+        Model1 = RandomForestClassifier(
+            n_estimators = self.attrs.rf_n_estimators,
+            max_depth = self.attrs.rf_max_depth,
+            min_samples_split = self.attrs.rf_min_samples_split,
+            min_samples_leaf = self.attrs.rf_min_samples_leaf,
+            random_state = 1,
+            n_jobs = self.attrs.rf_n_jobs)
+
+        Model1.fit(self.attrs.X_train, self.attrs.Y_train)
+
+        #Predict on X_train, X_test
+        Model1_pred_test = Model1.predict_proba(self.attrs.X_test)
+        Model1_pred_train = Model1.predict_proba(self.attrs.X_train)
+        Model1_pred_testsub = Model1.predict_proba(self.attrs.X_testsub)
+        Model1_pred_blindsub = Model1.predict_proba(self.attrs.X_blindsub)
+
+        #Build Model2 - Level 0
+        if verbose > 0:
+            print("Running SVM Classifier")
+
+        Model2 = SVC(C = self.attrs.svc_C, 
+                     gamma = self.attrs.svc_gamma, 
+                     kernel= self.attrs.svc_kernel, 
+                     probability = self.attrs.svc_probability,
+                     random_state = 2)
+        Model2.fit(self.attrs.X_train, self.attrs.Y_train)
+        #Predict on X_train, X_test
+        Model2_pred_test = Model2.predict_proba(self.attrs.X_test)
+        Model2_pred_train = Model2.predict_proba(self.attrs.X_train)
+        Model2_pred_testsub = Model2.predict_proba(self.attrs.X_testsub)
+        Model2_pred_blindsub = Model2.predict_proba(self.attrs.X_blindsub)
+
+        #Build Model3 - Level 0
+        if verbose > 0:
+            print("Quadratic Discriminant Analysis Classifier")
+
+        Model3 = QuadraticDiscriminantAnalysis()
+        Model3.fit(self.attrs.X_train, self.attrs.Y_train)
+        #Predict on X_train, X_test
+        Model3_pred_test = Model3.predict_proba(self.attrs.X_test)
+        Model3_pred_train = Model3.predict_proba(self.attrs.X_train)
+        Model3_pred_testsub = Model3.predict_proba(self.attrs.X_testsub)
+        Model3_pred_blindsub = Model3.predict_proba(self.attrs.X_blindsub)
+
+        #Build Model4 - Level 0
+        if verbose > 0:
+            print("GaussianNB Classifier")
+
+        Model4 = GaussianNB()
+        Model4.fit(self.attrs.X_train, self.attrs.Y_train)
+        #Predict on X_train, X_test
+        Model4_pred_test = Model4.predict_proba(self.attrs.X_test)
+        Model4_pred_train = Model4.predict_proba(self.attrs.X_train)
+        Model4_pred_testsub = Model4.predict_proba(self.attrs.X_testsub)
+        Model4_pred_blindsub = Model4.predict_proba(self.attrs.X_blindsub)
+
+        #Build Model5 - Level 0
+        if verbose > 0:
+            print("KNeighbors Classifier")
+
+        Model5 = KNeighborsClassifier(n_neighbors = self.attrs.kn_n_neighbors, 
+                                      weights = self.attrs.kn_weights)
+                            
+        Model5.fit(self.attrs.X_train, self.attrs.Y_train)
+        #Predict on X_train, X_test
+        Model5_pred_test = Model5.predict_proba(self.attrs.X_test)
+        Model5_pred_train = Model5.predict_proba(self.attrs.X_train)
+        Model5_pred_testsub = Model5.predict_proba(self.attrs.X_testsub)
+        Model5_pred_blindsub = Model5.predict_proba(self.attrs.X_blindsub)
+
+        #Final Model - Level 1 
+        #Creating training attributes for the stacked model
+        if verbose > 0:
+            print("Stacked Classifier")
+            
+        FeaturesTrain1 = np.hstack([Model1_pred_train,
+                                    Model2_pred_train,
+                                    Model3_pred_train,
+                                    Model4_pred_train,
+                                    Model5_pred_train])  
+        ModelFinal = LogisticRegression(random_state=49)
+        ModelFinal.fit(FeaturesTrain1, self.attrs.Y_train)
+
+        #Creating test attributes final model
+        Features_test1 = np.hstack([Model1_pred_test,
+                                    Model2_pred_test,
+                                    Model3_pred_test,
+                                    Model4_pred_test,
+                                    Model5_pred_test])
+        Features_testsub1 = np.hstack([Model1_pred_testsub,
+                                       Model2_pred_testsub,
+                                       Model3_pred_testsub,
+                                       Model4_pred_testsub,
+                                       Model5_pred_testsub])
+        Features_blindsub1 = np.hstack([Model1_pred_blindsub,
+                                        Model2_pred_blindsub,
+                                        Model3_pred_blindsub,
+                                        Model4_pred_blindsub,
+                                        Model5_pred_blindsub])
+
+
+        #Final predictions
+        self.attrs.final_pred = ModelFinal.predict_proba(Features_test1)
+        self.attrs.final_pred_testsub = ModelFinal.predict_proba(Features_testsub1)
+        self.attrs.final_pred_blindsub = ModelFinal.predict_proba(Features_blindsub1)
+
+        #AUC
+        if verbose > 0:
+            print("Calculating AUC")
+            
+        fpr, tpr, thresholds = roc_curve(self.attrs.Y_test, self.attrs.final_pred[:, 1])
+        roc_auc = auc(fpr, tpr)
+        print("AUC with Stacking: " , roc_auc)
+
+
+
---
+++ Submission/StackingAttributes.py
@@ -0,0 +1,272 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 14 06:35:28 2016
+
+@author: rob
+"""
+
+class StackingAttributes:
+
+    def __init__(self, X_train, Y_train, X_test, Y_test, X_testsub, X_blindsub):
+        self._rf_n_estimators = 100
+        self._rf_max_depth=175
+        self._rf_min_samples_split=9
+        self._rf_min_samples_leaf=1
+        self._rf_random_state = 1,
+        self._rf_n_jobs=-1
+        
+        self._svc_C = 10
+        self._svc_gamma=0.01
+        self._svc_kernel='rbf'
+        self._svc_probability=True
+        self._svc_random_state = 2,
+        self._svc_n_jobs=-1       
+        
+        self._kn_n_neighbors=15
+        self._kn_weights='distance'
+        self._kn_random_state = 2
+        self._kn_n_jobs=-1    
+                                      
+        self._X_train = X_train
+        self._X_test = X_test
+        self._X_testsub = X_testsub
+        self._X_blindsub = X_blindsub
+        self._Y_train = Y_train
+        self._Y_test = Y_test
+        
+        self._final_pred = None
+        self._final_pred_testsub = None
+        self._final_pred_blindsub = None
+
+    @property
+    def rf_n_estimators(self):
+        """"""
+        return self._rf_n_estimators
+
+    @rf_n_estimators.setter
+    def rf_n_estimators(self, value):
+        self._rf_n_estimators = value
+        
+    @property
+    def rf_max_depth(self):
+        """"""
+        return self._rf_max_depth
+
+    @rf_max_depth.setter
+    def rf_max_depth(self, value):
+        self._rf_max_depth = value
+        
+    @property
+    def rf_min_samples_split(self):
+        """"""
+        return self._rf_min_samples_split
+
+    @rf_min_samples_split.setter
+    def rf_min_samples_split(self, value):
+        self._rf_min_samples_split = value
+        
+    @property
+    def rf_min_samples_leaf(self):
+        """"""
+        return self._rf_min_samples_leaf
+
+    @rf_min_samples_leaf.setter
+    def rf_min_samples_leaf(self, value):
+        self._rf_min_samples_leaf = value
+        
+    @property
+    def rf_random_state(self):
+        """"""
+        return self._rf_random_state
+
+    @rf_random_state.setter
+    def rf_random_state(self, value):
+        self._rf_random_state = value
+        
+    @property
+    def rf_n_jobs(self):
+        """"""
+        return self._rf_n_jobs
+
+    @rf_n_jobs.setter
+    def rf_n_jobs(self, value):
+        self._rf_n_jobs = value
+        
+
+    @property
+    def svc_C(self):
+        """"""
+        return self._svc_C
+
+    @svc_C.setter
+    def svc_C(self, value):
+        self._svc_C = value
+        
+    @property
+    def svc_gamma(self):
+        """"""
+        return self._svc_gamma
+
+    @svc_gamma.setter
+    def svc_gamma(self, value):
+        self._svc_gamma = value
+        
+    @property
+    def svc_kernel(self):
+        """"""
+        return self._svc_kernel
+
+    @svc_kernel.setter
+    def svc_kernel(self, value):
+        self._svc_kernel = value
+        
+    @property
+    def svc_probability(self):
+        """"""
+        return self._svc_probability
+
+    @svc_probability.setter
+    def svc_probability(self, value):
+        self._svc_probability = value
+        
+        
+    @property
+    def svc_random_state(self):
+        """"""
+        return self._svc_random_state
+
+    @svc_random_state.setter
+    def svc_random_state(self, value):
+        self._svc_random_state = value
+        
+    @property
+    def svc_n_jobs(self):
+        """"""
+        return self._svc_n_jobs
+
+    @svc_n_jobs.setter
+    def svc_n_jobs(self, value):
+        self._svc_n_jobs = value
+   
+ 
+    @property
+    def kn_n_neighbors(self):
+        """"""
+        return self._kn_n_neighbors
+
+    @kn_n_neighbors.setter
+    def kn_n_neighbors(self, value):
+        self._kn_n_neighbors = value
+        
+    @property
+    def kn_weights(self):
+        """"""
+        return self._kn_weights
+
+    @kn_weights.setter
+    def kn_weights(self, value):
+        self._kn_weights = value
+        
+        
+    @property
+    def kn_random_state(self):
+        """"""
+        return self._kn_random_state
+
+    @kn_random_state.setter
+    def kn_random_state(self, value):
+        self._kn_random_state = value
+        
+    @property
+    def kn_n_jobs(self):
+        """"""
+        return self._kn_n_jobs
+
+    @kn_n_jobs.setter
+    def kn_n_jobs(self, value):
+        self._kn_n_jobs = value
+
+        
+    @property
+    def X_train(self):
+        """"""
+        return self._X_train
+
+    @X_train.setter
+    def X_train(self, value):
+        self._X_train = value
+
+    @property
+    def X_test(self):
+        """"""
+        return self._X_test
+
+    @X_test.setter
+    def X_test(self, value):
+        self._X_test = value
+        
+    @property
+    def X_testsub(self):
+        """"""
+        return self._X_testsub
+
+    @X_testsub.setter
+    def X_testsub(self, value):
+        self._X_testsub = value
+
+    @property
+    def X_blindsub(self):
+        """"""
+        return self._X_blindsub
+
+    @X_blindsub.setter
+    def X_blindsub(self, value):
+        self._X_blindsub = value
+
+        
+    @property
+    def Y_train(self):
+        """"""
+        return self._Y_train
+
+    @Y_train.setter
+    def Y_train(self, value):
+        self._Y_train = value
+        
+    @property
+    def Y_test(self):
+        """"""
+        return self._Y_test
+
+    @Y_test.setter
+    def Y_test(self, value):
+        self._Y_test = value
+        
+    @property
+    def final_pred(self):
+        """"""
+        return self._final_pred
+
+    @final_pred.setter
+    def final_pred(self, value):
+        self._final_pred = value
+        
+    @property
+    def final_pred_testsub(self):
+        """"""
+        return self._final_pred_testsub
+
+    @final_pred_testsub.setter
+    def final_pred_testsub(self, value):
+        self._final_pred_testsub = value
+        
+    @property
+    def final_pred_blindsub(self):
+        """"""
+        return self._final_pred_blindsub
+
+    @final_pred_blindsub.setter
+    def final_pred_blindsub(self, value):
+        self._final_pred_blindsub = value
+        
+        
